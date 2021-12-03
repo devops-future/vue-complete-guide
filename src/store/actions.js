2c89ab1ae7f7756f64aea0b94d93772326a0729e
devops-future <gice101582@gmail.com>
2021-12-03 15:21:20 +0800

    section 10 - Async / Await

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,25 +1,23 @@
 import {fetchList, fetchItemInfo, fetchUserInfo} from "../api";
 
 export default {
-  FETCH_LIST({commit}, pageName) {
-    fetchList(pageName)
-      .then(({data}) => {
-        commit('SET_LIST', data);
-      })
-      .catch();
+  async FETCH_LIST({commit}, pageName) {
+    const response = await fetchList(pageName);
+    commit('SET_LIST', response.data);
+    return response;
   },
-  FETCH_USER({commit}, name) {
-    fetchUserInfo(name)
-      .then(({data}) => {
-        commit('SET_USER', data);
-      })
-      .catch();
+  async FETCH_USER({commit}, name) {
+    const response = await fetchUserInfo(name);
+    commit('SET_USER', response.data);
+    return response;
   },
-  FETCH_ITEM({commit}, id) {
-    fetchItemInfo(id)
-      .then(({data}) => {
-        commit('SET_ITEM', data);
-      })
-      .catch();
+  async FETCH_ITEM({commit}, id) {
+    try {
+      const response = await fetchItemInfo(id);
+      commit('SET_ITEM', response.data);
+      return response;
+    } catch(error) {
+      console.log(error);
+    }
   }
 };
